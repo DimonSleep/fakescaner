ce494c8af3b4daf7cd8d4bb1cb277b194063845b
DimonSleep <andonidumitru1@gmail.com>
2024-11-02 18:33:39 +0200

    dablea

--- app.py
+++ app.py
@@ -5,16 +5,23 @@ import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem import WordNetLemmatizer
 import nltk
-from flask_cors import CORS
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
+import gdown  # pentru descărcarea modelului de pe Google Drive
 
-# Asigură-te că resursele necesare sunt descărcate
 nltk.download('wordnet')
 nltk.download('stopwords')
 
 app = Flask(__name__)
-CORS(app)
+
+# Descărcare model propagandă dacă nu este disponibil local
+def download_propaganda_model():
+    if not os.path.exists("distilbert_propaganda_model/model.safetensors"):
+        url = "https://drive.google.com/uc?id=1-7Wtfdj1qQM1qCVcbDdxsG1UT12y5o4s"  # ID-ul fișierului de pe Google Drive
+        os.makedirs("distilbert_propaganda_model", exist_ok=True)
+        gdown.download(url, "distilbert_propaganda_model/model.safetensors", quiet=False)
+
+download_propaganda_model()
 
 # Încărcăm vectorizatorul și modelul de detectare fake news
 with open('tfidf_vectorizer.pkl', 'rb') as f:
@@ -26,7 +33,7 @@ with open('ensemble_news_classifier_ro.pkl', 'rb') as f:
 lemmatizer = WordNetLemmatizer()
 
 # Încarcă modelul personalizat de propagandă și tokenizatorul
-propaganda_model_path = "./distilbert_propaganda_model"  # cale relativă fără simbolul punct
+propaganda_model_path = "distilbert_propaganda_model"
 propaganda_model = AutoModelForSequenceClassification.from_pretrained(propaganda_model_path)
 propaganda_tokenizer = AutoTokenizer.from_pretrained(propaganda_model_path)
 propaganda_model.eval()
